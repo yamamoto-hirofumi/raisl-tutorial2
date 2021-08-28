586f02aad4dc613f684649d4844bd8e8f9d631f8
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-08-28 06:30:14 +0000

    modeling-users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/sign_up',  to: 'users#new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
 end
