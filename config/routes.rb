292154798dcf0ed67830edf060ca5b985c53c93e
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-08-29 12:05:23 +0000

    basic-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/sign_up', to: 'users#new'
   post '/sign_up', to: 'users#create'
+  get    '/log_in',   to: 'sessions#new'
+  post   '/log_in',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 end
