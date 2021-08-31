7e6d9851b03f0f9eb3f062cd8a173024afa974dc
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-08-31 00:08:56 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,4 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,15 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+  
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post log_in_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 
 end
